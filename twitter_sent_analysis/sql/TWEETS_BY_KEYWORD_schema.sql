ceedcf3cae44e56c305e03dff7184646da93314a
SamSamhuns <samhunsadamant@gmail.com>
2019-10-27 23:39:59 +0400

    Add utf8mb4 encoding for MYSQL TABLE tweet COL

--- twitter_sent_analysis/sql/TWEETS_BY_KEYWORD_schema.sql
+++ twitter_sent_analysis/sql/TWEETS_BY_KEYWORD_schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE TWEETS_BY_KEYWORD (
     ID INT AUTO_INCREMENT,
     tweet_id VARCHAR(255) NOT NULL,
-    tweet TEXT NOT NULL,
+    tweet TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
     created_at VARCHAR(50),
     tweet_place VARCHAR(255),   /* Nullable object */
     favorite_count INT(11),       /* Nullable object */
